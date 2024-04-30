169e1d2d17b7f1f5a83bbddc96a5e0ae7bbd116b
Sebastian Ezequiel Hermo Avila <sebastianhermoavila@gmail.com>
2024-04-30 20:16:55 -0300

    Medio del medio TP

--- ejercicio/ejercicios.go
+++ ejercicio/ejercicios.go
@@ -1,24 +1,105 @@
 package ejercicio
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 var lista []*Ejercicio
 
 // Definición de la estructura de Ejercicio
 type Ejercicio struct {
-	Nombre         string
-	Descripcion    string
-	Tipo           []string
-	Dificultad     string
-	TiempoEstimado int // en segundos
-	Calorias       int
-	Puntos         int
+	Nombre      string
+	Descripcion string
+	Tipo        []string
+	Dificultad  string
+	Tiempo      int // en segundos
+	Calorias    int
+	Puntos      int
 }
 
 //Funciòn para crear un ejercicio nuevo por el usuario
 
 func CrearEjercicio() *Ejercicio {
-	fmt.Println("Creando Ejercicio")
+	fmt.Println("Creando Ejercicio...")
+	fmt.Print("Que Nombre deseas ponerle?:")
+	nombre := readLine()
+	fmt.Print("Algun detalle para recordarlo?:")
+	descipcion := readLine()
+	fmt.Print("Cuanto tiempo le vas a poner? (En segundos):")
+	var tiempo int
+	fmt.Scanln(&tiempo)
+	fmt.Print("Gasto estimado en calorias?:")
+	var calorias int
+	fmt.Scanln(&calorias)
+	fmt.Print("Dificultad? (Facil, Medio, Dificil):")
+	dificultad := readLine()
 
-	return nil
+	return &Ejercicio{
+		Nombre:      nombre,
+		Descripcion: descipcion,
+		Tiempo:      tiempo,
+		Calorias:    calorias,
+		Dificultad:  dificultad,
+	}
+}
+
+func NuevoEjercicio(nombre, descripcion string, dificultad string, tipo string, tiempo, calorias int, puntos int) *Ejercicio {
+
+	return &Ejercicio{
+		Nombre:      nombre,
+		Descripcion: descripcion,
+		Tiempo:      tiempo,
+		Calorias:    calorias,
+		Tipo:        []string{tipo},
+		Puntos:      puntos,
+		Dificultad:  dificultad,
+	}
+}
+
+func ListaPredefinida() []*Ejercicio {
+	s := NuevoEjercicio("sentadillas", "sube y baja con el abdomen", "facil", "flexibilidad", 40, 100, 30)
+	f := NuevoEjercicio("flexiones", "levantando el cuerpo únicamente con los brazos y bajando de nuevo al suelo",
+		"medio", "fuerza", 25, 250, 50)
+	j := NuevoEjercicio("jumping jack", "saltar mientras abres y cierras piernas y brazos",
+		"dificil", "cardio", 30, 560, 60)
+	z := NuevoEjercicio("zancadas alternas", "dar una zancada atrás y bajar la rodilla hasta que la pierna quede recta",
+		"medio", "flexibilidad", 12, 500, 20)
+	p := NuevoEjercicio("levantamiento de pesas", "agarrar pesas y subirlas y bajarlas con los brazos",
+		"medio", "fuerza", 30, 200, 20)
+	rutina := []*Ejercicio{s, f, j, z, p}
+	return rutina
+}
+
+func EntrenamientoEspartano() []*Ejercicio {
+	f := NuevoEjercicio("flexiones", "100 flexiones", "dificil", "fuerza", 1000, 500, 100)
+	a := NuevoEjercicio("abdominales", "100 abs", "dificil", "fuerza", 1000, 500, 100)
+	s := NuevoEjercicio("sentadillas", "100 sentadillas", "dificil", "flexibilidad", 150, 300, 100)
+	c := NuevoEjercicio("corre", "Corre por 10 kilometros", "dificil", "cardio", 300, 500, 100)
+
+	rutina := []*Ejercicio{f, a, s, c}
+	return rutina
+}
+
+func Añadir(eje *Ejercicio) {
+	lista = append(lista, eje)
+}
+
+func EliminarEjercico(nombre string) {
+	for i, eje := range Listado() {
+		if eje.Nombre == nombre {
+			lista = append(lista[:i], lista[i+1:]...)
+			fmt.Println("ejercio eliminad exitosamente.")
+			return
+		}
+	}
+	fmt.Println("No se encontró ninguna rutina con el nombre especificado.")
+}
+
+func readLine() string {
+	reader := bufio.NewReader(os.Stdin)
+	line, _ := reader.ReadString('\n')
+	return strings.TrimSpace(line)
 }
---
+++ rutina/rutinas.go
@@ -0,0 +1,140 @@
+package rutina
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	ej "github.com/tp-ayp2/ejercicio"
+)
+
+var Rutinas []*Rutina
+
+// Definición de la estructura de Rutina
+type Rutina struct {
+	Nombre     string
+	Ejercicios []*ej.Ejercicio
+	Duracion   int // en minutos
+	Calorias   int
+	Tipo       map[string]int
+	Dificultad string
+}
+
+// Función para eliminar una rutina del slice de rutinas
+func EliminarRutina(nombre string) {
+	for i, rutina := range Rutinas {
+		if rutina.Nombre == nombre {
+			Rutinas = append(Rutinas[:i], Rutinas[i+1:]...)
+			fmt.Println("Rutina eliminada exitosamente.")
+			return
+		}
+	}
+	fmt.Println("No se encontró ninguna rutina con el nombre especificado.")
+}
+
+// Función para consultar una rutina del slice de rutinas
+func ConsultarRutina(nombre string) *Rutina {
+	for _, rutina := range Rutinas {
+		if rutina.Nombre == nombre {
+			return rutina
+		}
+	}
+	fmt.Println("No se encontró ninguna rutina con el nombre especificado.")
+	return nil
+}
+
+// Función para crear una rutina predefinida y agregarla al slice de rutinas
+func CrearRutinaPredefinida() *Rutina {
+	var nombre string
+	fmt.Println("Nombre de la rutina predefinida: ")
+	fmt.Scanln(&nombre)
+
+	ejerciciosPredefinidos := ej.ListaPredefinida()
+	rutina := crearRutina(ejerciciosPredefinidos)
+	Rutinas = append(Rutinas, rutina)
+	rutina.Nombre = nombre
+	return rutina
+}
+
+func CrearRutinaDinamica() *Rutina {
+	fmt.Println("Ingrese el nombre de la rutina Dinamica: ")
+	var nombre string
+	fmt.Scanln(&nombre)
+
+	var cantidadEjercicios int
+	fmt.Print("Ingrese la cantidad de ejercicios: ")
+	fmt.Scanln(&cantidadEjercicios)
+
+	var ejercicios []*ej.Ejercicio
+	for i := 0; i < cantidadEjercicios; i++ {
+		ejercicio := ej.CrearEjercicio()
+		ejercicios = append(ejercicios, ejercicio)
+	}
+
+	rutina := crearRutina(ejercicios)
+	rutina.Nombre = nombre
+	Rutinas = append(Rutinas, rutina)
+
+	return rutina
+}
+
+func crearRutina(ejercicios []*ej.Ejercicio) *Rutina {
+	nuevaRutina := &Rutina{
+		Ejercicios: ejercicios,
+	}
+
+	for _, ejercicio := range ejercicios {
+		nuevaRutina.Duracion += ejercicio.TiempoEstimado
+		nuevaRutina.Calorias += ejercicio.Calorias
+	}
+
+	nuevaRutina.calcularDificultad()
+
+	return nuevaRutina
+}
+
+func (r *Rutina) calcularDificultad() {
+	var dificultadFacil, dificultadMedia, dificultadDificil int
+
+	for _, ejercicio := range r.Ejercicios {
+		switch ejercicio.Dificultad {
+		case "facil":
+			dificultadFacil++
+		case "medio":
+			dificultadMedia++
+		case "dificil":
+			dificultadDificil++
+		}
+	}
+
+	if dificultadDificil > dificultadMedia && dificultadDificil > dificultadFacil {
+		r.Dificultad = "dificil"
+	} else if dificultadMedia > dificultadDificil && dificultadMedia > dificultadFacil {
+		r.Dificultad = "medio"
+	} else {
+		r.Dificultad = "facil"
+	}
+}
+
+func MostrarRutina(rutina *Rutina) {
+	fmt.Println("Nombre de la Rutina:", rutina.Nombre)
+	fmt.Println("Duración Total:", rutina.Duracion, "minutos")
+	fmt.Println("Calorías Quemadas:", rutina.Calorias)
+	fmt.Println("Dificultad:", rutina.Dificultad)
+	fmt.Println("Ejercicios:")
+	for _, ejercicio := range rutina.Ejercicios {
+		fmt.Println("Nombre:", ejercicio.Nombre)
+		fmt.Println("Descripción:", ejercicio.Descripcion)
+		fmt.Println("Tiempo Estimado:", ejercicio.TiempoEstimado, "segundos")
+		fmt.Println("Calorías:", ejercicio.Calorias)
+		fmt.Println("Dificultad:", ejercicio.Dificultad)
+		fmt.Println()
+	}
+}
+
+func readLine() string {
+	reader := bufio.NewReader(os.Stdin)
+	line, _ := reader.ReadString('\n')
+	return strings.TrimSpace(line)
+}
