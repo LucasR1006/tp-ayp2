c454ecbbeebf3b421ad36702396fdfcd82b87545
LucasR1006 <lucasor1006@gmail.com>
2024-06-11 19:01:19 -0300

    arreglos basicos

---
+++ README.md
@@ -0,0 +1,6 @@
+### Disculpen los problemas
+
+Buenos dias/tardes/noches profes:
+    con mi compañero Lucas Rodriguez y yo Sebastian Ezequiel Hermo Avila, tuvimos un pequeño error con los grupos, estabamos confiados de que los 2 con los que estabamos antes perteneciamos en la misma comision y nos percatamos muy tarde de las cuestiones de quien estaba en tal comision al solo concentrarnos con el TP, por lo que entre Lucas y yo decidimos hacer otro TP solo entre nosotros para poder entregarles, desde ya disculpen las molestias y gracias por su atenciòn.
+
+atte: Lucas y Sebastian
---
+++ main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	ru "github.com/tp/rutina"
+)
+
+// Función principal
+func main() {
+
+	clear()
+	for {
+		fmt.Println("=========================================================")
+		fmt.Println("         MENÚ DE EJERCICIOS Y RUTINAS                    ")
+		fmt.Println("=========================================================")
+		fmt.Println("     1. Generar rutina basica")
+		fmt.Println("     2. Generar rutina espartana")
+		fmt.Println("     3. Generar rutina dinamica")
+		fmt.Println("     4. Opciones de rutinas")
+		fmt.Println("     0. Salir")
+		fmt.Println("=========================================================")
+		fmt.Print("Ingrese su opción: ")
+
+		var option int
+		fmt.Scanln(&option)
+
+		switch option {
+		case 0:
+			fmt.Println("=====================================")
+			fmt.Println("¡Hasta luego!")
+			fmt.Println("=====================================")
+			return
+		case 1:
+			fmt.Println("=====================================")
+			fmt.Println("Generando rutina predefinida...")
+			rutinaPredefinida := ru.CrearRutinaPredefinida()
+			fmt.Println(" ")
+			fmt.Println("Rutina creada:")
+			ru.MostrarRutina(rutinaPredefinida)
+			fmt.Println("=====================================")
+			break
+		case 2:
+			fmt.Println("=====================================")
+			fmt.Println("Generando rutina predefinida...")
+			rutinaPredefinida := ru.CrearRutinaEspartana()
+			fmt.Println(" ")
+			fmt.Println("Rutina creada:")
+			ru.MostrarRutina(rutinaPredefinida)
+			fmt.Println("=====================================")
+			break
+		case 3:
+			fmt.Println("=====================================")
+			fmt.Println("Generando rutina dinámica...")
+			rutinaDinamica := ru.CrearRutinaDinamica()
+			fmt.Println("Rutina creada:")
+			ru.MostrarRutina(rutinaDinamica)
+			fmt.Println("=====================================")
+			break
+		case 4:
+			otrasOpciones()
+			break
+		default:
+			fmt.Println("Opción inválida.")
+		}
+	}
+}
+
+func otrasOpciones() {
+	for {
+		fmt.Println("=========================================================")
+		fmt.Println("         OPCIONES DE RUTINAS        ")
+		fmt.Println("=========================================================")
+		fmt.Println("     1. Eliminar rutina")
+		fmt.Println("     2. Consultar rutina")
+		fmt.Println("     3. Mostrar todas las rutinas")
+		fmt.Println("     0. Volver atras")
+		fmt.Println("=========================================================")
+		fmt.Print("Ingrese una opción: ")
+
+		var option int
+		fmt.Scanln(&option)
+
+		switch option {
+		case 0:
+			return
+		case 1:
+			fmt.Println("==================================================")
+			fmt.Print("Ingrese el nombre de la rutina que desea eliminar: ")
+			var nombre string
+			fmt.Scanln(&nombre)
+			ru.EliminarRutina(nombre)
+			fmt.Println("==================================================")
+			break
+		case 2:
+			fmt.Println("==================================================")
+			fmt.Print("Ingrese el nombre de la rutina que desea consultar: ")
+			var nombre string
+			fmt.Scanln(&nombre)
+			rutina := ru.ConsultarRutina(nombre)
+			if rutina != nil {
+				ru.MostrarRutina(rutina)
+			}
+			fmt.Println("==================================================")
+			break
+		case 3:
+			fmt.Println("==================================================")
+			fmt.Println("Listado de todas las rutinas:")
+			for _, rutina := range ru.Rutinas {
+				fmt.Println("-", rutina.Nombre)
+			}
+			fmt.Println("==================================================")
+			break
+		default:
+			fmt.Println("Opción inválida.")
+		}
+	}
+}
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------------------------
+func clear() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func readLine() string {
+	reader := bufio.NewReader(os.Stdin)
+	line, _ := reader.ReadString('\n')
+	return strings.TrimSpace(line)
+}
